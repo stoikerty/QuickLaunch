3cfd88037f602c35130b944ce23d27e97bf69dec
Jorge Antunes <ask@stoikerty.com>
2025-02-17 20:23:47 +0000

    prevent creating another tab if the current tab is already a new tab

--- extensionTemplates/background.js
+++ extensionTemplates/background.js
@@ -6,3 +6,19 @@ chrome.browserAction.onClicked.addListener(() => {
     chrome.tabs.create({ url });
   });
 });
+
+chrome.browserAction.onClicked.addListener(() => {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    const currentTab = tabs[0];
+    // Define common new tab URLs for Chromium-based browsers.
+    const newTabUrls = ['chrome://newtab/', 'chrome://newtab', 'about:newtab', 'about:newtab/'];
+
+    if (currentTab && newTabUrls.includes(currentTab.url)) {
+      // If current tab is a new tab, update it with the target URL.
+      chrome.tabs.update(currentTab.id, { url: DEFAULT_URL });
+    } else {
+      // Otherwise, open the target URL in a new tab.
+      chrome.tabs.create({ url: DEFAULT_URL });
+    }
+  });
+});
